95d146858dd6369822e88a8a854def8622f653f8
Denis Kiselev <kiselevden911@yandex.ru>
2025-04-06 12:58:59 +0500

    add .php-cs-fixer.dist.php

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+$config = (new Config())
+    ->setFinder(
+        Finder::create()
+            ->in(__DIR__ . '/src')
+            ->in(__DIR__ . '/bin')
+            ->in(__DIR__ . '/tests')
+            ->append([
+                __FILE__,
+            ])
+            ->exclude([
+                'Fixtures',
+            ]),
+    )
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setCacheFile(__DIR__ . '/var/' . basename(__FILE__) . '.cache')
+    ->setRules([
+        '@PSR12' => true,
+    ]);
+
+return $config;
